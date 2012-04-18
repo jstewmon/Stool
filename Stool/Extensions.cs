996d8d676a1af3a4891102c38218770fcaeefcb6
Jonathan Stewmon <jstewmon@gmail.com>
2012-04-18 17:36:32 -0500

    added some logging

--- Stool/Extensions.cs
+++ Stool/Extensions.cs
@@ -1,20 +1,25 @@
 ﻿using System.Web;
 using Newtonsoft.Json;
+using log4net;
 
 namespace Stool
 {
     public static class Extensions
     {
+        private static ILog _log = LogManager.GetLogger(typeof (Extensions));
+
         public static void Send<T>(this HttpContext context, T data)
         {
             context.Send(data, 200);
         }
         public static void Send<T>(this HttpContext context, T data, int code)
         {
+            _log.Debug("Sending response with status " + code);
             context.Response.StatusCode = code;
             context.Response.ContentType = "application/json";
             var serializer = new JsonSerializer();
             serializer.Serialize(context.Response.Output, data);
+            _log.Debug("Sent response with status " + code);
         }
     }
 }
--- Stool/Middleware.cs
+++ Stool/Middleware.cs
@@ -7,11 +7,14 @@ using System.Threading;
 using System.Web;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using log4net;
 
 namespace Stool
 {
     public static class Middleware
     {
+        private static ILog _log = LogManager.GetLogger(typeof(Middleware));
+
         /// <summary>
         /// Loads body into a <see cref="JObject"/> and adds the result into <paramref name="ctx.Items"/> with the key "body"
         /// </summary>
@@ -19,17 +22,34 @@ namespace Stool
         /// <param name="next"></param>
         public static void ParseBody(HttpContext ctx, Action next)
         {
+            _log.Debug("Begin ParseBody");
             if (ctx.Request.HttpMethod != "POST")
             {
+                _log.DebugFormat("httpMethod is {0}, calling next()", ctx.Request.HttpMethod);
                 next();
+                _log.Debug("End ParseBody");
                 return;
             }
 
             using (var reader = new StreamReader(ctx.Request.InputStream))
             {
-                ctx.Items.Add("body", JObject.Load(new JsonTextReader(reader)));
+                try
+                {
+                    ctx.Items.Add("body", JObject.Load(new JsonTextReader(reader)));
+                    _log.Debug("ParseBody successfully parsed the request body");
+                }
+                catch (Exception ex)
+                {
+                    _log.Error(ex);
+                    ctx.Request.InputStream.Seek(0, SeekOrigin.Begin);
+                    using (var r = new StreamReader(ctx.Request.InputStream))
+                        _log.Error(r.ReadToEnd());
+                    throw;
+                }
             }
+            _log.Debug("ParseBody calling next()");
             next();
+            _log.Debug("End ParseBody");
         }
     }
 }
